66156aae0bbfbbcfa179d735a4da85da692f5eb6
Fabio <stella.vlassi01@gmail.com>
2023-11-30 23:12:58 +0200

    frist_draft_of_frontend

---
+++ app.mjs
@@ -0,0 +1,25 @@
+import express from 'express';
+import { engine } from 'express-handlebars'
+
+const app = express();
+const PORT = process.env.PORT || '3000';
+
+app.use(express.static('public'));
+
+app.engine('hbs',engine({ extname: 'hbs' }))
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    try {
+        console.log("okay")
+        res.render('layouts/main');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+
+const server = app.listen(PORT, () => {
+    console.log(`http://127.0.0.1:${PORT}`);
+});
