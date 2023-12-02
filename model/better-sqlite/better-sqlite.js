7dd0d436f97b34f31eb29f21db859701c2e149cf
eliannapot <up1072682@upnet.gr>
2023-12-02 16:48:35 +0200

    added things that are relevant

--- model/better-sqlite/better-sqlite.js
+++ model/better-sqlite/better-sqlite.js
@@ -1,4 +1,41 @@
 const sqlite = require('better-sqlite3');
 const db = new sqlite('../database/smartparking.sqlite');
 
-//insert test
+//insert test from db to front
+// 'use strict';
+
+// import db from 'better-sqlite3'
+// const sql = new db('model/database/gymspot.sqlite', { fileMustExist: true });
+// // Τι χρειαζομαι το const sql = new db....
+
+// export let readUser = (user) => {
+//     const stmt = sql.prepare('SELECT * FROM User task WHERE username = ?');
+//     let info;
+//     try {
+//         info = stmt.all(user.username);
+//         return true;
+//     }
+//     catch (err) {
+//         throw err;
+//     }
+// }
+
+//test from front to db (?)
+// 'use strict';
+
+// import db from 'better-sqlite3'
+// const sql = db('../database/gymspot.sqlite');
+// // Τι χρειαζομαι το const sql = new db....
+
+// export let addUser = (user) => {
+//     const stmt = sql.prepare('INSERT INTO User VALUES (?, ?, ?, 0)');
+//     let info;
+
+//     try {
+//         info = stmt.run(user.username, user.email, user.password);
+//         return true;
+//     }
+//     catch (err) {
+//         throw err;
+//     }
+// }
