5b01345416e1ed6c6645f802d450d3134ae4ebf2
Fabio <stella.vlassi01@gmail.com>
2023-12-02 16:55:28 +0200

    small changes in home

--- app.mjs
+++ app.mjs
@@ -1,23 +1,35 @@
 import express from 'express';
-import { engine } from 'express-handlebars'
+import { engine } from 'express-handlebars';
+import router from './routes/router.mjs';
 
 const app = express();
 const PORT = process.env.PORT || '3000';
 
 app.use(express.static('public'));
 
-app.engine('hbs',engine({ extname: 'hbs' }))
+app.engine('hbs', engine({ extname: 'hbs' }))
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
     try {
-        res.render('layouts/main');
+        res.render('home');
     } catch (error) {
         console.error(error);
         res.status(500).send('Internal Server Error');
     }
 });
 
+// New route for home_faculty.hbs
+app.get('/home_faculty', (req, res) => {
+    try {
+        res.render('home_faculty');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.use('/',router);
 
 const server = app.listen(PORT, () => {
     console.log(`http://127.0.0.1:${PORT}`);
---
+++ routes/router.mjs
@@ -0,0 +1,31 @@
+import express from 'express'
+
+const router = express.Router()
+
+router.get('/', (req,res) => {
+    res.redirect('/home')
+});
+
+router.get('/home', (req, res) => {
+    try {
+        res.render('home');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.get('/home_faculty', (req, res) => {
+    const faculty="ECE"
+    try {
+        console.log("my faculty is",faculty)
+        res.render('home_faculty',{
+            faculty: faculty})
+        } 
+        catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+export default router
