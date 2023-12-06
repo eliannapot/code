451c7b176e96a21222acce562ea55feb708b4b30
Fabio <stella.vlassi01@gmail.com>
2023-12-06 17:10:26 +0200

    changed faculty&area to site

--- public/script.js
+++ public/script.js
@@ -1,23 +1,23 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     var select = document.getElementById("parkingSpot");
+document.addEventListener("DOMContentLoaded", function () {
+    var select = document.getElementById("parkingSpot");
 
-//     // Add the default option
-//     var defaultOption = document.createElement("option");
-//     defaultOption.value = "";
-//     defaultOption.text = "Select a spot";
-//     defaultOption.disabled = true;
-//     defaultOption.selected = true;
-//     select.appendChild(defaultOption);
+    // Add the default option
+    var defaultOption = document.createElement("option");
+    defaultOption.value = "";
+    defaultOption.text = "Select a spot";
+    defaultOption.disabled = true;
+    defaultOption.selected = true;
+    select.appendChild(defaultOption);
 
-//     // Generate options from Spot 1 to Spot 16
-//     for (var i = 1; i <= 16; i++) {
-//         var option = document.createElement("option");
-//         option.value = i;
-//         option.text = "Spot " + i;
-//         option.style.fontSize = "16px";
-//         select.appendChild(option);
-//     }
-// });
+    // Generate options from Spot 1 to Spot 16
+    for (var i = 1; i <= 16; i++) {
+        var option = document.createElement("option");
+        option.value = i;
+        option.text = "Spot " + i;
+        option.style.fontSize = "16px";
+        select.appendChild(option);
+    }
+});
 
 
 
@@ -28,7 +28,7 @@ window.onload = function () {
         if (form.checkValidity()) {
             // Check if the current page is /home before redirecting
             if (window.location.pathname === '/home') {
-                window.location.href = "/home_faculty";
+                window.location.href = "/home_site";
             } else {
                 // Handle the case where the form should not be submitted on other pages
                 alert("Form submission is allowed only on the /home page.");
--- routes/router.mjs
+++ routes/router.mjs
@@ -21,26 +21,24 @@ router.get('/home', async (req,res) => {
     }
 });
 
-
-
 //ΠΡΟΒΛΗΜΑ: 
 //*ΌΤΑΝ ΕΙΜΑΙ ΣΤΟ "/": ΕΝΩ ΚΑΝΕΙ RENDER ΣΤΟ HOME.HBS ΔΕΝ ΚΑΝΕΙ ΚΑΝΕΝΑ CONSOLE LOG ΑΡΑ ΔΕΝ ΔΟΥΛΕΥΕΙ Ο ΚΩΔΙΚΑΣ ΤΟΥ /HOME
 //*ΌΤΑΝ ΕΙΜΑΙ ΣΤΟ "/HOME":ΔΟΥΛΕΥΕΙ ΤΕΛΕΙΑ ΑΛΛΑ ΠΡΕΠΕΙ ΝΑ ΤΟ ΠΑΤΑΩ ΧΕΙΡΟΚΙΝΗΤΑ
 //* ΑΝ ΚΡΑΤΗΣΩ ΜΟΝΟ ΤΟ "/" ΚΑΙ ΒΑΛΩ ΕΚΕΙ ΤΙΣ ΕΝΤΟΛΕΣ, ΕΚΤΕΛΕΙ ΤΟ RENDER ΑΛΛΑ ΟΧΙ ΤΑ CONSOLE LOG!
 //VS CODE ΓΑΜΙΕΣΑΙ
 
-router.get('/home_faculty', (req, res) => {
-    const faculty="ECE"
-    console.log("my faculty is",faculty)
-    res.render('home_faculty',{
-        faculty: faculty})
-});
+// router.get('/home_faculty', (req, res) => {
+//     const faculty="ECE"
+//     console.log("my faculty is",faculty)
+//     res.render('home_faculty',{
+//         faculty: faculty})
+// });
 
-router.get('/home_area', (req, res) => {
-    const area="1"
+router.get('/home_site', (req, res) => {
+    const site="ECE_1"
     try {
-        res.render('home_area',{
-            area: area})
+        res.render('home_site',{
+            site: site})
         } 
         catch (error) {
         console.error(error);
