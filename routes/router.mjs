7883c4553e768c90479675d37ce5ec74072b70a3
Fabio <stella.vlassi01@gmail.com>
2023-12-13 13:06:48 +0200

    without console log

--- routes/router.mjs
+++ routes/router.mjs
@@ -7,10 +7,8 @@ router.get('/', (req, res) => {
 });
 
 router.get('/home', async (req,res) => {
-    console.log('This should always log');
     try{
         const site="ECE_1"
-        console.log("my site is",site);
         res.render('home',{
             site: site
         });
@@ -21,21 +19,7 @@ router.get('/home', async (req,res) => {
     }
 });
 
-//ΠΡΟΒΛΗΜΑ: 
-//*ΌΤΑΝ ΕΙΜΑΙ ΣΤΟ "/": ΕΝΩ ΚΑΝΕΙ RENDER ΣΤΟ HOME.HBS ΔΕΝ ΚΑΝΕΙ ΚΑΝΕΝΑ CONSOLE LOG ΑΡΑ ΔΕΝ ΔΟΥΛΕΥΕΙ Ο ΚΩΔΙΚΑΣ ΤΟΥ /HOME
-//*ΌΤΑΝ ΕΙΜΑΙ ΣΤΟ "/HOME":ΔΟΥΛΕΥΕΙ ΤΕΛΕΙΑ ΑΛΛΑ ΠΡΕΠΕΙ ΝΑ ΤΟ ΠΑΤΑΩ ΧΕΙΡΟΚΙΝΗΤΑ
-//* ΑΝ ΚΡΑΤΗΣΩ ΜΟΝΟ ΤΟ "/" ΚΑΙ ΒΑΛΩ ΕΚΕΙ ΤΙΣ ΕΝΤΟΛΕΣ, ΕΚΤΕΛΕΙ ΤΟ RENDER ΑΛΛΑ ΟΧΙ ΤΑ CONSOLE LOG!
-//VS CODE ΓΑΜΙΕΣΑΙ
-
-// router.get('/home_faculty', (req, res) => {
-//     const faculty="ECE"
-//     console.log("my faculty is",faculty)
-//     res.render('home_faculty',{
-//         faculty: faculty})
-// });
-
 router.get('/home_site', (req, res) => {
-    const site="ECE_1"
     try {
         res.render('home_site',{
             site: site})
