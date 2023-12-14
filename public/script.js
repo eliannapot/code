fd20c3d5ce5625e745d6c000060906c253229da0
Fabio <stella.vlassi01@gmail.com>
2023-12-14 20:44:22 +0200

    trynna fix redirection button in home page

--- public/script.js
+++ public/script.js
@@ -27,40 +27,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
 //redirect from /home to /home_site when clicking on the "Continue" button
 
-// document.addEventListener("DOMContentLoaded", function () {
-//     var button = document.getElementById('submitButton');
-//     if (button) {
-//         button.addEventListener('click', submitForm);
-//     }
-// });
-
 function submitForm() {
     var form = document.getElementById('bookingForm');
-    var select = document.getElementById('parkingSpot');
-    console.log("Form Submited");
-    // Check if the elements are present
-    if (!form || !select) {
-        return;
-    }
-
-    // Check if the parking spot is selected
-    if (!select.value) {
-        alert("Please select a parking spot.");
-        return;
-    }
 
     if (form.checkValidity()) {
-        // Check if the current page is /home before redirecting
-        if (window.location.pathname === '/home') {
-            window.location.href = "/home_site";
-        } else {
-            // Handle the case where the form should not be submitted on other pages
-            alert("Form submission is allowed only on the /home page.");
-        }
+        window.location.href = '/home_site';
     } else {
-        // The form is not valid, you can provide feedback to the user or take other actions
         alert("Please fill in all the required fields.");
     }
+    
 }
 
 
